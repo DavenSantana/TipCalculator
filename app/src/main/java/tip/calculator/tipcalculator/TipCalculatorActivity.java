a5a3a5b413ae84914c3b4340cf80a97224c881ed
Daven <daven.santana38@qmail.cuny.edu>
2021-12-01 15:02:48 -0500

    Commiting Tip Calculator

---
+++ app/src/main/java/tip/calculator/tipcalculator/TipCalculatorActivity.java
@@ -0,0 +1,86 @@
+package tip.calculator.tipcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class TipCalculatorActivity extends AppCompatActivity {
+
+    private EditText checkAmountValue;
+    private EditText partySizeValue;
+    private Button buttonCompute;
+    private EditText fifteenPercentTipValue;
+    private EditText twentyPercentTipValue;
+    private EditText twentyfivePercentTipValue;
+    private EditText fifteenPercentTotalValue;
+    private EditText twentyPercentTotalValue;
+    private EditText twentyfivePercentTotalValue;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkAmountValue = findViewById(R.id.checkAmountValue);
+        partySizeValue = findViewById(R.id.partySizeValue);
+        buttonCompute = findViewById(R.id.buttonCompute);
+        fifteenPercentTipValue = findViewById(R.id.fifteenPercentTipValue);
+        twentyPercentTipValue = findViewById(R.id.twentyPercentTipValue);
+        twentyfivePercentTipValue = findViewById(R.id.twentyfivePercentTipValue);
+        fifteenPercentTotalValue = findViewById(R.id.fifteenPercentTotalValue);
+        twentyPercentTotalValue = findViewById(R.id.twentyPercentTotalValue);
+        twentyfivePercentTotalValue = findViewById(R.id.twentyfivePercentTotalValue);
+
+        buttonCompute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate();
+            }
+        });
+    }
+
+    private void calculate(){
+
+        Double copyCheckAmountValue;
+        Double copyPartySizeValue;
+        long percentTipValue15;
+        long percentTipValue20;
+        long percentTipValue25;
+        Double percentTotalValue15;
+        Double percentTotalValue20;
+        Double percentTotalValue25;
+
+        if(checkAmountValue.getText().toString().equals("") || partySizeValue.getText().toString().equals("") || Double.parseDouble(checkAmountValue.getText().toString()) <= 0 || Double.parseDouble( partySizeValue.getText().toString()) <= 0 ){
+
+            Toast.makeText(this, "Empty or incorrect value(s)!", Toast.LENGTH_SHORT).show();
+        }
+
+        if(!checkAmountValue.getText().toString().equals("")){
+
+            copyCheckAmountValue = Double.parseDouble(checkAmountValue.getText().toString());
+            copyPartySizeValue = Double.parseDouble(partySizeValue.getText().toString());
+
+            percentTotalValue15 =  (copyCheckAmountValue * 15 / 100);
+            percentTotalValue20 = (copyCheckAmountValue * 20 / 100);
+            percentTotalValue25 = (copyCheckAmountValue * 25 / 100);
+
+            percentTipValue15 = Math.round(percentTotalValue15 / copyPartySizeValue);
+            percentTipValue20 = Math.round(percentTotalValue20 / copyPartySizeValue);
+            percentTipValue25 = Math.round(percentTotalValue25 / copyPartySizeValue);
+
+            fifteenPercentTotalValue.setText(String.valueOf(percentTotalValue15.intValue()));
+            twentyPercentTotalValue.setText(String.valueOf(percentTotalValue20.intValue()));
+            twentyfivePercentTotalValue.setText(String.valueOf(percentTotalValue25.intValue()));
+
+            fifteenPercentTipValue.setText(String.valueOf(percentTipValue15));
+            twentyPercentTipValue.setText(String.valueOf(percentTipValue20));
+            twentyfivePercentTipValue.setText(String.valueOf(percentTipValue25));
+
+        }
+    }
+}
